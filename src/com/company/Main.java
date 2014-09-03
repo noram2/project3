187e853e96cdb9556f05184fc8e8ecd29b25f457
noram2 <norairismitchell@yahoo.com>
2014-09-03 16:33:43 -0400

    hi dad :D

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        File file = new File(“../data/myfile.txt”);
         try(BufferedReader br = new BufferedReader(new FileReader(file))) {
             for(String line; (line = br.readLine()) != null; ) {
                 // process the line.
