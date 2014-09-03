147fe3aecdf3d7a08d0aa5a15f2b2aa6a2de01c6
noram2 <norairismitchell@yahoo.com>
2014-09-03 17:16:10 -0400

    hi dad :D

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,19 +6,13 @@ import java.io.FileReader;
 
 public class Main {
 
-    public static void main(String[] args) {
-        File file = new File("../data/myfile.txt");
-        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
-            for(String line; (line = br.readLine()) != null; ) {
-                // process the line.
-
-            }
-            // line is not visible here.
-
-        }
-        catch (Exception e) {
-            // don’t worry about exceptions for now
-        }
-    }
-}
-
+        public static void main(String[] args) {
+                File file = new File(“../data/myfile.txt”);
+                try {
+                       BufferedReader br = new BufferedReader(new FileReader(file)));
+                        for(String line; (line = br.readLine()) != null; ) {
+                                // process the line.
+                            }
+                        // line is not visible here.
+                    }
+            }
