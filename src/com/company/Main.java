61cd3e9ee3552f78b4bb9c1b690d99e6e9686d60
noram2 <norairismitchell@yahoo.com>
2014-09-03 17:17:42 -0400

    hi dad :D

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,10 +9,11 @@ public class Main {
         public static void main(String[] args) {
                 File file = new File(“../data/myfile.txt”);
                 try {
-                       BufferedReader br = new BufferedReader(new FileReader(file)));
+                       BufferedReader br = new BufferedReader(new FileReader(file));
                         for(String line; (line = br.readLine()) != null; ) {
                                 // process the line.
                             }
                         // line is not visible here.
                     }
             }
+}
