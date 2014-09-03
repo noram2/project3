dc024903c080314eae0b94a4a72d6959e672f79c
noram2 <norairismitchell@yahoo.com>
2014-09-03 16:04:34 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            for(String line; (line = br.readLine()) != null; ) {
+                // process the line.
+            }
+            // line is not visible here.
+        }
+    }
+}
+
